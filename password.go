e2d604ecae8b9563c5c211e2c451d767eaf4ab6e
AIZAWA Hina <hina@bouhime.com>
2019-03-25 14:16:12 +0900

    Fix naming rule about acronyms (eg Md5 -> MD5)

--- password.go
+++ password.go
@@ -13,8 +13,8 @@ type Password struct {
 	plain string
 }
 
-// Md5Hash ... MD5 ハッシュ化パスワードを生成
-func (p Password) Md5Hash() string {
+// MD5Hash ... MD5 ハッシュ化パスワードを生成
+func (p Password) MD5Hash() string {
 	if str, err := p.cryptHash(crypt.MD5.New(), "$1$"+p.salt(8)); err != nil {
 		panic(err)
 	} else {
@@ -22,8 +22,8 @@ func (p Password) Md5Hash() string {
 	}
 }
 
-// Sha256Hash ... SHA-256 ハッシュ化パスワードを生成
-func (p Password) Sha256Hash() string {
+// SHA256Hash ... SHA-256 ハッシュ化パスワードを生成
+func (p Password) SHA256Hash() string {
 	if str, err := p.cryptHash(crypt.SHA256.New(), "$5$"+p.salt(16)); err != nil {
 		panic(err)
 	} else {
@@ -31,8 +31,8 @@ func (p Password) Sha256Hash() string {
 	}
 }
 
-// Sha512Hash ... SHA-512 ハッシュ化パスワードを生成
-func (p Password) Sha512Hash() string {
+// SHA512Hash ... SHA-512 ハッシュ化パスワードを生成
+func (p Password) SHA512Hash() string {
 	if str, err := p.cryptHash(crypt.SHA512.New(), "$6$"+p.salt(16)); err != nil {
 		panic(err)
 	} else {
@@ -51,9 +51,9 @@ func (p Password) BcryptHash() string {
 
 func (p Password) String() string {
 	return p.plain + "\n" +
-		"    MD5:     " + p.Md5Hash() + "\n" +
-		"    SHA-256: " + p.Sha256Hash() + "\n" +
-		"    SHA-512: " + p.Sha512Hash() + "\n" +
+		"    MD5:     " + p.MD5Hash() + "\n" +
+		"    SHA-256: " + p.SHA256Hash() + "\n" +
+		"    SHA-512: " + p.SHA512Hash() + "\n" +
 		"    Bcrypt:  " + p.BcryptHash()
 }
 
